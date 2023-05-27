b2fd0c5745132d63740113a2f199cbfeb3558258
ndanilova <70074699+ndanilova@users.noreply.github.com>
2023-05-27 16:26:27 +0300

    Add files via upload

---
+++ lab5/JDBCLabs/src/JDBCTest.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCTest {
+    public static void main(String[] args) {
+        Connection conn = null;
+        //Class.forName(org.apache.derby.jdbc.ClientDriver);
+        /*conn = DriverManager.getConnection(jdbc:derby://localhost:1527/JavaTunesDB);*/
+    }
+}
